c66bded23e3162a8bf24f00a921bfc984b24da9b
Tomasz Łasica <tlasica@life.pl>
2015-07-10 12:50:38 -0400

    assembly updated and solved deduplication issues

--- project/build.scala
+++ project/build.scala
@@ -1,7 +1,6 @@
 import sbt._
 import Keys._
-import sbtassembly.Plugin._
-import AssemblyKeys._
+import sbtassembly.AssemblyPlugin.autoImport._
 
 
 object build extends Build {
@@ -9,15 +8,18 @@ object build extends Build {
 
     lazy val libAkkaActors = "com.typesafe.akka" %% "akka-actor" % "2.3.6"
 
-    def standardSettings = Defaults.defaultSettings ++ src ++ assemblySettings ++ Seq (
-        mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) => {
+
+    def standardSettings = Defaults.defaultSettings ++ src ++ Seq (
+        scalaVersion := compilerVersion,
+        assemblyMergeStrategy in assembly := {
             case "plugin.properties" => MergeStrategy.first
             case "about.html" => MergeStrategy.first
-            case x => old(x)
-          }
+            case x =>
+                val oldStrategy = (assemblyMergeStrategy in assembly).value
+                oldStrategy(x)
         }
-      ,  scalaVersion := compilerVersion
-    ) ++ implVersion
+    )
+
 
     lazy val implVersion = Seq (
         packageOptions <<= (version) map {
@@ -70,8 +72,8 @@ object build extends Build {
                 "org.eclipse.jgit" % "org.eclipse.jgit.http.server" % "1.3.0.201202151440-r",
 		            "org.scalatest" %% "scalatest" % "2.2.4" % "test",
                 "org.testng" % "testng" % "6.5.1" % "test",
-		            "org.specs2" % "specs2_2.10" % "2.4.17",
-                "org.specs2" %% "specs2-scalaz-core" % "7.0.0"
+		            "org.specs2" % "specs2_2.10" % "2.4.17" % "test",
+                "org.specs2" %% "specs2-scalaz-core" % "7.0.0" % "test"
             ),
             resolvers ++= Seq("JGit Repository" at "http://download.eclipse.org/jgit/maven",
             "Scalaz Bintray Repo"  at "http://dl.bintray.com/scalaz/releases")
