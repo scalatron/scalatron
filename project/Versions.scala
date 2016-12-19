51947257b082ab73c009c156ae8f08e545882b62
Joachim Hofer <joachim.hofer@zalando.de>
2016-12-19 16:57:51 +0100

    #6 extract dependencies (and versions) into separate files

---
+++ project/Versions.scala
@@ -0,0 +1,11 @@
+object Versions {
+  val Akka              = "2.4.14"
+  val HttpClient        = "4.5.2"
+  val Jackson           = "2.8.5"
+  val Jersey            = "1.19.3"
+  val Jetty             = "7.6.21.v20160908"
+  val JGit              = "4.5.0.201609210915-r"
+  val ParserCombinators = "1.0.4"
+  val Scalatest         = "3.0.1"
+  val ServletApi        = "2.5"
+}
